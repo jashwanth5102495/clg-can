d8c7a1581f330d110b6c5e4cb662492dfc7dc46c
Jashwanth5102495 <112746469+jashwanth5102495@users.noreply.github.com>
2025-09-24 18:11:33 +0530

    Revert "new one"
    This reverts commit 237024ec01f9c9aea5bb2c9a04517bf050b83bab.

--- Desktop/final year projects/Car Rental Booking System/backend/models/associations.js
+++
@@ -1,9 +0,0 @@
-// MongoDB/Mongoose models
-// Relationships are defined in the schemas using ObjectId references
-
-import User from './User.js';
-import Car from './Car.js';
-import Booking from './Booking.js';
-import Payment from './Payment.js';
-
-export { User, Car, Booking, Payment };
